365b004bce314a7aebc230a4a0bd834c2e35234f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-05-31 15:07:54 +0200

    ui. added dashboard view with charts

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -25,7 +25,172 @@
     <h2>{{ $t('dashboard.title') }}</h2>
 
     <div v-if="!view.isLoaded" class="spinner spinner-lg view-spinner"></div>
-    <div v-show="view.isLoaded"></div>
+    <div v-show="view.isLoaded">
+      <div class="row">
+        <h3>{{ $t('dashboard.ipsec_stats') }}</h3>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.ipsec.connected}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.ipsec_connected') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span class="card-pf-utilization-card-details-count stats-count">{{status.ipsec.total}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.ipsec_total') }}</span>
+          </span>
+        </div>
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="row">
+        <h3>{{ $t('dashboard.openvpn_tun_stats') }}</h3>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.tunnels.connected}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_tun_connected') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.tunnels.total}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_tun_total') }}</span>
+          </span>
+        </div>
+        <!-- charts -->
+        <h4>{{ $t('dashboard.openvpn_tun_interfaces') }}</h4>
+        <div
+          v-for="(i,ik) in status.openvpn.tunnels.interfaces"
+          :key="ik"
+          class="col-xs-6 col-sm-6 col-md-6 col-lg-6"
+        >
+          <h5>{{i}}</h5>
+          <div v-if="!charts[i]" class="spinner spinner-lg view-spinner"></div>
+          <div
+            v-if="charts[i] && charts[i].data && charts[i].data.length == 0"
+            class="empty-piechart"
+          >
+            <span class="fa fa-line-chart"></span>
+            <div>{{ $t('dashboard.empty_piechart_label') }}</div>
+          </div>
+          <div v-show="charts[i] && charts[i].data && charts[i].data.length > 0">
+            <h4 class="col-sm-12">
+              <div :id="'ovpn-tun-legend-'+i" class="legend"></div>
+            </h4>
+            <div :id="'ovpn-tun-chart-'+i" class="col-sm-12"></div>
+          </div>
+        </div>
+        <!-- end charts -->
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="row">
+        <h3>{{ $t('dashboard.openvpn_rw_stats') }}</h3>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.roadwarrior.connected}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_connected') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.roadwarrior.total}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_total') }}</span>
+          </span>
+        </div>
+
+        <div class="stats-container col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.roadwarrior.auth | capitalize}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_auth') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.roadwarrior.mode | capitalize}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_mode') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <span
+            class="card-pf-utilization-card-details-count stats-count"
+          >{{status.openvpn.roadwarrior.port}}</span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_port') }}</span>
+          </span>
+        </div>
+        <div class="stats-container col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <span class="card-pf-utilization-card-details-count stats-count">
+            <span
+              :class="status.openvpn.roadwarrior.status == 'enabled' ? 'fa fa-check green' : 'fa fa-times red'"
+            ></span>
+          </span>
+          <span class="card-pf-utilization-card-details-description stats-description">
+            <span
+              class="card-pf-utilization-card-details-line-2 stats-text"
+            >{{ $t('dashboard.openvpn_rw_status') }}</span>
+          </span>
+        </div>
+        <!-- charts -->
+        <h4>{{ $t('dashboard.openvpn_rw_interfaces') }}</h4>
+        <div
+          v-for="(i,ik) in status.openvpn.roadwarrior.interfaces"
+          :key="ik"
+          class="col-xs-6 col-sm-6 col-md-6 col-lg-6"
+        >
+          <h5>{{i}}</h5>
+          <div v-if="!charts[i]" class="spinner spinner-lg view-spinner"></div>
+          <div
+            v-if="charts[i] && charts[i].data && charts[i].data.length == 0"
+            class="empty-piechart"
+          >
+            <span class="fa fa-line-chart"></span>
+            <div>{{ $t('dashboard.empty_piechart_label') }}</div>
+          </div>
+          <div v-show="charts[i] && charts[i].data && charts[i].data.length > 0">
+            <h4 class="col-sm-12">
+              <div :id="'ovpn-rw-legend-'+i" class="legend"></div>
+            </h4>
+            <div :id="'ovpn-rw-chart-'+i" class="col-sm-12"></div>
+          </div>
+        </div>
+        <!-- end charts -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,19 +198,200 @@
 var nethserver = window.nethserver;
 var console = window.console;
 
-import generatePieChart from "@/piechart";
+import Dygraph from "dygraphs";
 
 export default {
   name: "Dashboard",
-  mounted() {},
+  beforeRouteLeave(to, from, next) {
+    for (var i in this.charts) {
+      if (this.charts[i].pollingId) {
+        clearInterval(this.charts[i].pollingId);
+      }
+    }
+    next();
+  },
+  mounted() {
+    this.getStats();
+  },
   data() {
     return {
       view: {
         isLoaded: false
-      }
+      },
+      status: {
+        openvpn: {
+          tunnels: {
+            interfaces: [],
+            connected: 0,
+            total: 0
+          },
+          roadwarrior: {
+            auth: "certificate",
+            interfaces: [],
+            mode: "routed",
+            status: "enabled",
+            connected: 0,
+            port: 0,
+            total: 0
+          }
+        },
+        ipsec: {
+          connected: 0,
+          total: 0
+        }
+      },
+      charts: {}
     };
   },
-  methods: {}
+  methods: {
+    initCharts(chartName, type) {
+      var context = this;
+
+      context.view.isChartLoaded = false;
+      nethserver.exec(
+        ["nethserver-vpn/dashboard/read"],
+        {
+          action: "chart",
+          name: chartName
+        },
+        null,
+        function(success) {
+          try {
+            success = JSON.parse(success);
+          } catch (e) {
+            console.error(e);
+          }
+
+          if (success.data && success.data.length > 0) {
+            for (var t in success.data) {
+              success.data[t][0] = new Date(success.data[t][0] * 1000);
+            }
+
+            context.charts[chartName] = new Dygraph(
+              document.getElementById("ovpn-" + type + "-chart-" + chartName),
+              success.data.reverse(),
+              {
+                fillGraph: true,
+                stackedGraph: true,
+                labels: success.labels,
+                height: 150,
+                strokeWidth: 1,
+                strokeBorderWidth: 1,
+                ylabel:
+                  (context.$i18n && context.$i18n.t("dashboard.connections")) ||
+                  "Connections",
+                axisLineColor: "white",
+                labelsDiv: document.getElementById(
+                  "ovpn-" + type + "-legend-" + chartName
+                ),
+                labelsSeparateLines: true,
+                legend: "always",
+                axes: {
+                  y: {
+                    axisLabelFormatter: function(y) {
+                      return Math.ceil(y);
+                    }
+                  }
+                }
+              }
+            );
+            context.charts[chartName].data = success.data;
+
+            setTimeout(function() {
+              context.charts[chartName].resize();
+            }, 100);
+
+            // start polling
+            if (!context.charts[chartName].pollingId) {
+              context.charts[chartName].pollingId = setInterval(function() {
+                context.updateCharts(chartName, type);
+              }, 1000);
+            }
+            context.$forceUpdate();
+          } else {
+            context.charts[chartName] = {
+              data: []
+            };
+            context.$forceUpdate();
+          }
+        },
+        function(error) {
+          console.error(error);
+          context.view.isChartLoaded = true;
+        }
+      );
+    },
+    updateCharts(chartName) {
+      var context = this;
+      nethserver.exec(
+        ["nethserver-vpn/dashboard/read"],
+        {
+          action: "chart",
+          name: chartName
+        },
+        null,
+        function(success) {
+          try {
+            success = JSON.parse(success);
+          } catch (e) {
+            console.error(e);
+          }
+          if (success.data.length > 0) {
+            for (var t in success.data) {
+              success.data[t][0] = new Date(success.data[t][0] * 1000);
+            }
+
+            context.charts[chartName].updateOptions({
+              file: success.data.reverse()
+            });
+          } else {
+            context.charts[chartName] = {
+              data: []
+            };
+            context.$forceUpdate();
+          }
+        },
+        function(error) {
+          console.error(error);
+        }
+      );
+    },
+    getStats() {
+      var context = this;
+
+      context.view.isLoaded = false;
+      nethserver.exec(
+        ["nethserver-vpn/dashboard/read"],
+        {
+          action: "status"
+        },
+        null,
+        function(success) {
+          try {
+            success = JSON.parse(success);
+          } catch (e) {
+            console.error(e);
+          }
+          context.status = success;
+
+          for (var i in success.openvpn.tunnels.interfaces) {
+            var name = success.openvpn.tunnels.interfaces[i];
+            context.initCharts(name, "tun");
+          }
+
+          for (var i in success.openvpn.roadwarrior.interfaces) {
+            var name = success.openvpn.roadwarrior.interfaces[i];
+            context.initCharts(name, "rw");
+          }
+
+          context.view.isLoaded = true;
+        },
+        function(error) {
+          console.error(error);
+        }
+      );
+    }
+  }
 };
 </script>
 
@@ -70,5 +416,44 @@ export default {
 
 .mg-bottom-10 {
   margin-bottom: 10px;
+}
+
+.status-body {
+  font-size: 14px;
+}
+
+.row {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.semi-bold {
+  font-weight: 500;
+}
+
+.stats-count-small {
+  margin-right: 5px;
+  font-size: 18px;
+  font-weight: 300;
+  float: left;
+  line-height: 0.5;
+}
+
+.stats-text-small {
+  line-height: 0.5;
+}
+
+.legend {
+  z-index: 5;
+}
+
+.stats-description-small {
+  float: left;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.divider {
+  margin-top: 20px;
 }
 </style>
